00919cfa394c54d3d71a8974554f61e2eb02f758
DemonLives <32984250+DankerDev@users.noreply.github.com>
2017-11-11 18:10:05 -0500

    predictive shit

--- behaviors/BehaviorDb.AncientRuins.cs
+++ behaviors/BehaviorDb.AncientRuins.cs
@@ -42,7 +42,7 @@ namespace wServer.logic
                     new Prioritize(
                         new Follow(1.2, range: 0)
                         ),
-                    new Shoot(15, 4, 15, projectileIndex: 1, predictive: 1, coolDown: 500),
+                    new Shoot(15, 4, 15, projectileIndex: 1, predictive: 0.2, coolDown: 500),
                     new Spawn("Sand Golem", 2, 1, coolDown: 5000),
                     new HpLessTransition(0.65, "phase2")
                     ),
@@ -77,7 +77,7 @@ namespace wServer.logic
                     new Shoot(20, 4, shootAngle: 20, projectileIndex: 4, angleOffset: 160, coolDown: 1800),
                     new Shoot(20, 4, shootAngle: 20, projectileIndex: 0, angleOffset: 240, coolDown: 2200),
                     new Shoot(20, 4, shootAngle: 20, projectileIndex: 4, angleOffset: 320, coolDown: 2600),
-                    new Shoot(15, 3, 7, projectileIndex: 3, predictive: 1, coolDown: 1500),
+                    new Shoot(15, 3, 7, projectileIndex: 3, coolDown: 1500),
                     new TimedTransition(20000, "suicide")
                     ),
                 new State("suicide",
@@ -104,7 +104,7 @@ namespace wServer.logic
                     new Prioritize(
                         new Wander(0.4)
                         ),
-                    new Shoot(20, 2, 10, 0, predictive: 1, coolDown: 1000),
+                    new Shoot(20, 2, 10, 0, coolDown: 1000),
                     new Spawn("Sand Crawler", 2, 1, coolDown: 10000),
                     new EntityExistsTransition("Sand General", 15, "protect")
                     ),
@@ -117,7 +117,7 @@ namespace wServer.logic
                     new Prioritize(
                         new Follow(0.4, range: 3)
                         ),
-                    new Shoot(20, 2, 10, 0, predictive: 1, coolDown: 1000),
+                    new Shoot(20, 2, 10, 0, predictive: 0.31, coolDown: 1000),
                     new Spawn("Sand Crawler", 2, 1, coolDown: 5000),
                     new TimedTransition(10000, "protect")
                     )
@@ -138,7 +138,7 @@ namespace wServer.logic
                     new Wander(0.6),
                     new Follow(1.5, range: 3)
                     ),
-                new Shoot(10, 3, 15, 0, predictive: 1, coolDown: 1000)
+                new Shoot(10, 3, 15, 0, predictive: 0, coolDown: 1000)
                 )
             )
         .Init("Sand Slurp",
@@ -146,7 +146,7 @@ namespace wServer.logic
                 new Prioritize(
                     new Follow(0.7, 15, 15)
                     ),
-                new Shoot(15, 1, projectileIndex: 0, predictive: 1, coolDown: 500)
+                new Shoot(15, 1, projectileIndex: 0, predictive: 0.1, coolDown: 500)
                 )
             )
         .Init("Sand Maggot",
@@ -160,7 +160,7 @@ namespace wServer.logic
                     new Prioritize(
                         new Wander(0.5)
                         ),
-                    new Shoot(15, 3, 20, projectileIndex: 0, predictive: 1, coolDown: 1500)
+                    new Shoot(15, 3, 20, projectileIndex: 0, predictive: 0, coolDown: 1500)
                     )
                 )
             )
@@ -168,7 +168,7 @@ namespace wServer.logic
             new State(
                 new State("defend",
                     new Orbit(0.7, 10, 4, "Sand Maggot"),
-                    new Shoot(7, 1, projectileIndex: 0, predictive: 1, coolDown: 2000)
+                    new Shoot(7, 1, projectileIndex: 0, predictive: 0.3, coolDown: 2000)
                     )
                 )
             )
@@ -177,7 +177,7 @@ namespace wServer.logic
                 new Prioritize(
                     new Wander(0.4)
                     ),
-                new Shoot(10, 2, 10, 0, predictive: 1, coolDown: 400)
+                new Shoot(10, 2, 10, 0, predictive: 0.5, coolDown: 400)
                 )
             );
     }
